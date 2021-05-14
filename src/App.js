024986328e3d588d98f7f8a052bae8f58293a3a5
Akshay Adme <akshayadme@gmail.com>
2021-05-14 12:08:16 +0530

    Nav component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Navs from './Components/Navs';
+import Home from './Components/Pages/Home';
+import Starred from './Components/Pages/Starred';
+
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" />
-      <Route>404 page</Route>
-    </Switch>
+    <>
+      <Navs />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/starred" component={Starred} />
+        <Route>404 page</Route>
+      </Switch>
+    </>
   );
 }
 
---
+++ src/Components/Pages/Starred.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Starred = () => {
+  return (
+    <div>
+      <h1>starred</h1>
+    </div>
+  );
+};
+
+export default Starred;
