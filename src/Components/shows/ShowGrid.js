f9573523347a23cfa968c4e8a89336593e293ecb
Akshay Adme <akshayadme@gmail.com>
2021-05-14 19:43:55 +0530

    starred functionality implemented

--- src/Components/shows/ShowGrid.js
+++ src/Components/shows/ShowGrid.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ShowCard from './ShowCard';
 import notFound from '../../images/not-found.png';
 import { FlexGrid } from '../styled';
+import { useShows } from '../misc/custom-hooks';
 
 const ShowGrid = ({ data }) => {
+  const [starredShows, dispatchStarred] = useShows();
+
   return (
     <FlexGrid>
       {data.map(({ show }) => {
+        const isStarred = starredShows.includes(show.id);
+        const onStarredClick = () => {
+          if (isStarred) {
+            dispatchStarred({ type: 'REMOVE', showId: show.id });
+          } else {
+            dispatchStarred({ type: 'ADD', showId: show.id });
+          }
+        };
         return (
           <ShowCard
             key={show.id}
@@ -14,6 +25,8 @@ const ShowGrid = ({ data }) => {
             name={show.name}
             image={show.image ? show.image.medium : notFound}
             summary={show.summary}
+            onStarredClick={onStarredClick}
+            isStarred={isStarred}
           />
         );
       })}
